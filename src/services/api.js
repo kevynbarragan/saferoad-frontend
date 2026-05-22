47e48b5331f2f95b592fafc28e13d83fce48515f
Kevyn <kevinbarragan315@gmail.com>
2026-05-22 13:07:24 -0500

    Fix production API URL and cleanup

--- src/services/api.js
+++ src/services/api.js
@@ -1,96 +1,32 @@
-const BASE = "https://saferoad-api-6z17.onrender.com"
+const BASE = 'https://saferoad-api-6z17.onrender.com'
 
-export async function fetchSectores(){
-
-const res=await fetch(
-`${BASE}/sectores`
-)
-
-return res.json()
-
+export async function fetchSectores() {
+  const res = await fetch(`${BASE}/sectores`)
+  return res.json()
 }
 
-
-export async function fetchResumen(){
-
-const res=await fetch(
-`${BASE}/resumen`
-)
-
-return res.json()
-
+export async function fetchResumen() {
+  const res = await fetch(`${BASE}/resumen`)
+  return res.json()
 }
 
-
-export async function fetchDepartamentos(){
-
-const res=await fetch(
-`${BASE}/departamentos`
-)
-
-return res.json()
-
+export async function fetchDepartamentos() {
+  const res = await fetch(`${BASE}/departamentos`)
+  return res.json()
 }
 
-
-export async function fetchTopCiudades(){
-
-const res=await fetch(
-`${BASE}/top-ciudades`
-)
-
-return res.json()
-
+export async function fetchTopCiudades() {
+  const res = await fetch(`${BASE}/top-ciudades`)
+  return res.json()
 }
 
+export async function fetchFiltro(departamento, municipio, muertes) {
+  const params = new URLSearchParams()
 
-export async function fetchFiltro(
+  if (departamento) params.append('departamento', departamento)
+  if (municipio) params.append('municipio', municipio)
+  if (muertes) params.append('muertes', muertes)
 
-departamento,
-municipio,
-muertes
-
-){
-
-const params=new URLSearchParams()
-
-
-if(departamento){
-
-params.append(
-'departamento',
-departamento
-)
-
-}
-
-
-if(municipio){
-
-params.append(
-'municipio',
-municipio
-)
-
-}
-
-
-if(muertes){
-
-params.append(
-'muertes',
-muertes
-)
-
-}
-
-
-const res=await fetch(
-
-`${BASE}/filtrar?${params}`
-
-)
-
-return res.json()
-
+  const res = await fetch(`${BASE}/filtrar?${params}`)
+  return res.json()
 }
